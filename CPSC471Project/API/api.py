ac615af92984f41908ca050b2d9216e503e86121
Evyn710 <evyn710@gmail.com>
2021-11-22 17:25:41 -0700

    addded an dump of the current database and also added more to the api

--- CPSC471Project/API/api.py
+++ CPSC471Project/API/api.py
@@ -1,9 +1,12 @@
+import requests
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 import mysql.connector
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 api = Api(app)
+bcrypt = Bcrypt(app)
 
 # initializing mysql
 con = mysql.connector.connect(user='user', password='password', host='127.0.0.1', database='rentalcompany')
@@ -11,28 +14,65 @@ con = mysql.connector.connect(user='user', password='password', host='127.0.0.1'
 rental_put_args = reqparse.RequestParser()
 rental_put_args.add_argument("RegNo", type=int, help="Should be null for new, regNo to overwrite")
 
+# helper function to return what kind of user the user is
+def validateUser(Username, Password):
+    cursor = con.cursor(dictionary=True)
+
+    # find out if they are user
+    cursor.execute("SELECT Username, Password FROM account WHERE Username = %s", (Username,))
+    user = cursor.fetchone()
+    if user and bcrypt.check_password_hash(user['Password'], Password):
+        return "user"
+    else:
+        return 401
 
 class AvailableRentals(Resource):
     def get(self):  # get method for getting all rentals that are available
         cursor = con.cursor(dictionary=True)
         cursor.execute("SELECT Make, Model, Color, City, Address FROM rental WHERE Status = 'available'")
         data = cursor.fetchall()
+        cursor.close()
         return data
 
 
-class RentCar(Resource):
+class AvailableRentalCar(Resource):
     def get(self, RegNo):
         cursor = con.cursor(dictionary=True)
         num = cursor.execute("SELECT Make, Model, Color, City, Address FROM rental WHERE Status = 'available' and RegNo = " + str(RegNo))
         data = cursor.fetchone()
+        cursor.close()
         return data
 
-    def put(self, RegNo):
-        return request.json
+
+class AvailableRentalsCity(Resource):
+    def get(self, city):
+        cursor = con.cursor(dictionary=True)
+        num = cursor.execute("SELECT Make, Model, Color, City, Address FROM rental WHERE City = %s", (city,))
+        data = cursor.fetchall()
+        cursor.close()
+        return data
 
 
+class CurrentRentalsClient(Resource):
+    def get(self):
+        # check input
+        keys = list(dict(request.json).keys())
+        if keys[0] != 'Username' or keys[1] != 'Password' or len(keys) > 2:
+            return 'Invalid input', 400
+
+        validation = validateUser(request.json['Username'], request.json['Password'])
+        if validation == 401:
+            #return authorization error
+            return 'Invalid user', 401
+        else:
+            pass
+
+
+# adding each resource as an endpoint
 api.add_resource(AvailableRentals, "/api/rentals")
-api.add_resource(RentCar, "/api/rentals/<int:RegNo>")
+api.add_resource(AvailableRentalCar, "/api/rentals/<int:RegNo>")
+api.add_resource(AvailableRentalsCity, "/api/<city>/rentals")
+api.add_resource(CurrentRentalsClient, "/api/user/rentals")
 
 
 @app.route('/')
