b61f272d3e22d01f81bce6cd5e462b6f91acbd96
john-abo <john.abo@ucalgary.ca>
2021-12-11 06:14:27 -0700

    Completed endpoint skeletons
    There's still plenty that needs to be resolved. The queries haven't been tested as the database needs to be setup first. Some clarification needed for some queries regarding what the blueprint meant by certain values. These should be minor fixes, and we should be able to finish on time for demonstration.

--- CPSC471Project/API/api.py
+++ CPSC471Project/API/api.py
@@ -108,10 +108,11 @@ class ClientRentHistory(Resource):
         # I'm not entirely familiar with how to get input from user, this may need to be fixed
         
         cursor = con.cursor(dictionary=True)
-        # SELECT Make, Model, Color, City, Address
-        #   FROM Rents
-        #       WHERE Customer_id = %s (customer_id,)
-        num = cursor.execute("SELECT Make, Model, Color, City, Address FROM RENTS WHERE Customer_id = %s", (customer_id,))
+        # SELECT R.Make, R.Model, R.Color, R.City, R.Address
+        #   FROM Rents as C, Rental as R
+        #       WHERE C.Customer_id = %s (customer_id,) and
+        #               R.Reg# = C.Reg#
+        num = cursor.execute("SELECT R.Make, R.Model, R.Color, R.City, R.Address FROM Rents as C, Rental as R WHERE C.Customer_id = %s and R.Reg# = C.Reg#", (customer_id,))
         data = cursor.fetchall()
         cursor.close()
         return data
@@ -133,7 +134,7 @@ class ClientInsurancePlans(Resource):
         return data
 
 # EP7
-class AgentGetRentalStatuses(Resource):
+class EmployeeGetRentalStatuses(Resource):
     def get(self):
         # TODO, insert user verification with ID
 
@@ -146,7 +147,7 @@ class AgentGetRentalStatuses(Resource):
         return data
 
 # EP8
-class AgentGetRentalStatus(Resource):
+class EmployeeGetRentalStatus(Resource):
     def get(self, RegNo):
         # TODO, insert user verification with ID
 
@@ -155,11 +156,119 @@ class AgentGetRentalStatus(Resource):
         #       WHERE Reg# = %s (RegNo,)
         cursor = con.cursor(dictionary=True)
         num = cursor.execute("SELECT * FROM Rental WHERE Reg# = %s", (RegNo,))
-        data = cursor.fetchall()
+        data = cursor.fetchone()
         cursor.close()
         return data
 
 # EP9
+class EmployeeGetAllGarages(Resource):
+    def get(self):
+        # TODO, insert user verification with ID
+
+        # SELECT *  -- Pretty much the same as EP7, but I think this is fine as it is
+        #   FROM Garage
+        cursor = con.cursor(dictionary=True)
+        num = cursor.execute("SELECT * FROM Garage")
+        data = cursor.fetchall()
+        cursor.close()
+        return data
+
+# EP10
+class EmployeeGetAllShuttles(Resource):
+    def get(self,GarageNo):
+        # TODO, insert user verification with ID
+
+        # I need to clarify which capacity the end point is referring to
+        # SELECT GS.Shuttle#, G.City, G.Address, COUNT(GS.Shuttle#), G.Capacity -- Idk about this count, it's says current capacity and total capacity but I'm a bit confused with which is stored in Garage
+        #   FROM Garage_Shuttle as GS, Garage as G
+        #       WHERE GS.Garage# = G.Garage#
+        cursor = con.cursor(dictionary=True)
+        num = cursor.execute("")
+        data = cursor.fetchall()
+        cursor.close()
+        return data
+    
+# EP11
+class EmployeeAddRental(Resource):
+    def post(self):
+        new_rental = request.json
+
+        # Checks the agent's input
+        # I think I still need to check ALL the keys, but we can add that later
+        keys = list(dict(request.json).keys())
+        if keys[0] != 'Username' or keys[1] != 'Password' or len(keys) > 2:
+            return 'Invalid input', 400
+
+        # It seems there's a constraint with the city and address of the rental, where they need to exist at a location
+        # So just need to check that
+        # SELECT City, Address
+        #   FROM Location
+        #       WHERE City = '%s' and Address = '%s' (new_rental['City'],new_rental['Address'])
+        cursor = con.cursor(dictionary=True)
+        cursor.execute("SELECT City, Address FROM Location WHERE City = '%s' and Address = '%s'", (new_rental['City'],new_rental['Address']))
+        check = cursor.fetchall()
+
+        if check:
+            # INSERT INTO RENTAL (Color, Status, Make, Model, City, Address)
+            #   VALUES (%s, %s, %s, %s, %s, %s) (you get the idea, this is new_rental's keys)
+            # Defined the tuple here to ensure the line wasn't too long
+            atts = (new_rental['Color'], new_rental['Status'], new_rental['Make'], new_rental['Model'], new_rental['City'], new_rental['Address'])
+            cursor.execute("INSERT INTO RENTAL (Color, Status, Make, Model, City, Address) VALUES (%s, %s, %s, %s, %s, %s)", atts)
+            cursor.commit()
+            cursor.close()  # Not sure if this needs to be closed after a commit, but adding just in case
+            return "Rental added"
+        else:
+            cursor.close()
+            return "Location is not valid.", 400
+
+# EP12
+class EmployeeUpdateRentalStatus(Resource):
+    def put(self):
+        updated_rental = request.json
+
+        # Checks the agent's input
+        # I think I still need to check ALL the keys, but we can add that later
+        keys = list(dict(request.json).keys())
+        if keys[0] != 'Username' or keys[1] != 'Password' or len(keys) > 2:
+            return 'Invalid input', 400
+
+        # UPDATE Rental
+        #   SET Status='%s'
+        #       WHERE Reg#=%s
+        cursor = con.cursor(dictionary=True)
+        cursor.execute("UPDATE Rental SET Status='%s' WHERE Reg#=%s")
+        cursor.close()
+        return "Rental status updated"
+
+# EP13
+class EmployeeGetCustomerRental(Resource):
+    def get(self, customer_id):
+        # TODO, insert user verification with ID
+
+        # SELECT R.Reg#, R.Make, R.Model, R.Color, R.City, R.Address, R.Status
+        #   FROM Rental as R, Rents as C
+        #       WHERE   C.Customer_id='%s' and
+        #               C.Reg# = R.Reg# (customer_id,)
+        cursor = con.cursor(dictionary=True)
+        num = cursor.execute("SELECT R.Reg#, R.Make, R.Model, R.Color, R.City, R.Address, R.Status FROM Rental as R, Rents as C WHERE   C.Customer_id='%s' and C.Reg# = R.Reg#")
+        data = cursor.fetchall()
+        cursor.close()
+        return data
+
+# EP14
+class EmployeeGetHours(Resource):
+    def get(self):
+        # TODO, insert user verification with ID
+
+        # SELECT E.Hours
+        #   FROM Employee as E, Account as A    -- Going to assume that the account username is accessable
+        #       WHERE E.Username = A.Username and
+        #               E.SSN = ''-- I'm not sure how to get this data just yet
+        cursor = con.cursor(dictionary=True)
+        num = cursor.execute("")
+        data = cursor.fetchall()
+        cursor.close()
+        return data
 
 # adding each resource as an endpoint
 api.add_resource(AvailableRentals, "/api/rentals")
@@ -169,9 +278,12 @@ api.add_resource(CurrentRentalsClient, "/api/user/rentals")
 api.add_resource(NewUser, "/api/newuser")
 api.add_resource(ClientRentHistory, "/api/user/rentals")
 api.add_resource(ClientInsurancePlans, "/api/user/plans")
-api.add_resource(AgentGetRentalStatuses, "/api/rentals_status")
-api.add_resource(AgentGetRentalStatus, "/api/rentals_status/<int:RegNo>")
-
+api.add_resource(EmployeeGetRentalStatuses, "/api/rentals_status")
+api.add_resource(EmployeeGetRentalStatus, "/api/rentals_status/<int:RegNo>")
+api.add_resource(EmployeeGetAllGarages, "/api/garages")
+api.add_resource(EmployeeGetAllShuttles, "/api/garages/<int:garageNo>")
+api.add_resource(EmployeeAddRental, "/api/rentals")
+api.add_resource(EmployeeUpdateRentalStatus, "/api/rentals/<int:RegNo>")
 
 @app.route('/')
 @app.route('/api')
