faebad28fc7a6950ae6fd9bad5d7c30327159cec
Evyn710 <evyn710@gmail.com>
2021-12-13 18:21:00 -0700

    Added an account page to website as well as the manage link, also finished verify_user

--- CPSC471Project/API/api.py
+++ CPSC471Project/API/api.py
@@ -16,15 +16,43 @@ rental_put_args.add_argument("RegNo", type=int, help="Should be null for new, re
 
 
 # helper function to return what kind of user the user is
-def validateUser(username, password):
+# helper function to return what kind of user the user is
+def validate_user(username, password):
     cursor = con.cursor(dictionary=True)
 
-    # find out if they are user
+    # find out if they are a user
     cursor.execute("SELECT Username, Password FROM account WHERE Username = %s", (username,))
     user = cursor.fetchone()
-    cursor.close()
-    if user and bcrypt.check_password_hash(user['Password'], password):
-        return "user"
+    if user and bcrypt.check_password_hash(password, user["Password"]):
+        # check if they are an employee
+        cursor.execute("SELECT Username FROM employee WHERE Username = %s", (username,))
+        user = cursor.fetchone()
+        if user:  # they are an employee
+            # check if they are an agent
+            cursor.execute("SELECT Agent_SSN FROM agent as a, employee as e WHERE " +
+                           "a.Agent_SSN = e.SSN and e.Username = %s", (username,))
+            agent = cursor.fetchone()
+            if agent:
+                return "agent"
+
+            # check if they are a mechanic
+            cursor.execute("SELECT Mechanic_SSN FROM mechanic as m, employee as e WHERE " +
+                           "m.Mechanic_SSN = e.SSN and e.Username = %s", (username,))
+            mechanic = cursor.fetchone()
+            if mechanic:
+                return "mechanic"
+
+            # check if they are a manager
+            cursor.execute("SELECT SSN FROM employee as e WHERE e.Username = %s", (username,))
+            SSN = cursor.fetchone()
+            cursor.execute("SELECT * FROM employee as e WHERE e.Mgr_ssn = %s", (SSN,))
+            manages = cursor.fetchone()
+            if manages:
+                return "manager"
+
+            return "employee"
+        else:
+            return "user"
     else:
         return 401
 
@@ -41,7 +69,7 @@ class AvailableRentals(Resource):
 class AvailableRentalCar(Resource):
     def get(self, RegNo):
         cursor = con.cursor(dictionary=True)
-        num = cursor.execute("SELECT Make, Model, Color, City, Address FROM rental WHERE Status = 'available' and RegNo = " + str(RegNo))
+        num = cursor.execute("SELECT Make, Model, Color, City, Address FROM rental WHERE Status = 'available' and RegNo = %s", (RegNo,))
         data = cursor.fetchone()
         cursor.close()
         return data
--- CPSC471Project/WebApp/rentalApp/user.py
+++ CPSC471Project/WebApp/rentalApp/user.py
@@ -1,9 +1,10 @@
 from flask_login import UserMixin
-from rentalApp import con
+from rentalApp import con, bcrypt
 
 
 class User(UserMixin):
     def __init__(self, username, password):
+        self.usertype = validate_user(username, password)
         self.username = username
         self.password = password
 
@@ -20,3 +21,44 @@ class User(UserMixin):
             return User(data["Username"], data["Password"])
 
         return None;
+
+
+# helper function to return what kind of user the user is
+def validate_user(username, password):
+    cursor = con.cursor(dictionary=True)
+
+    # find out if they are a user
+    cursor.execute("SELECT Username, Password FROM account WHERE Username = %s", (username,))
+    user = cursor.fetchone()
+    if user and user['Password'] == password:
+        # check if they are an employee
+        cursor.execute("SELECT Username FROM employee WHERE Username = %s", (username,))
+        user = cursor.fetchone()
+        if user:  # they are an employee
+            # check if they are an agent
+            cursor.execute("SELECT Agent_SSN FROM agent as a, employee as e WHERE " +
+                           "a.Agent_SSN = e.SSN and e.Username = %s", (username,))
+            agent = cursor.fetchone()
+            if agent:
+                return "agent"
+
+            # check if they are a mechanic
+            cursor.execute("SELECT Mechanic_SSN FROM mechanic as m, employee as e WHERE " +
+                           "m.Mechanic_SSN = e.SSN and e.Username = %s", (username,))
+            mechanic = cursor.fetchone()
+            if mechanic:
+                return "mechanic"
+
+            # check if they are a manager
+            cursor.execute("SELECT SSN FROM employee as e WHERE e.Username = %s", (username,))
+            SSN = cursor.fetchone()
+            cursor.execute("SELECT * FROM employee as e WHERE e.Mgr_ssn = %s", (SSN,))
+            manages = cursor.fetchone()
+            if manages:
+                return "manager"
+
+            return "employee"
+        else:
+            return "user"
+    else:
+        return 401
